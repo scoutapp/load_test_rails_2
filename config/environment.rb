d30a3867b78bb5a50cda9c4c921164c3f96fe3e9
Andre Lewis <andre@highgroove.com>
2016-06-09 15:07:19 -0600

    allow latest version of scout_apm gem

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "scout_apm", :version => "1.5.2.pre"
+  config.gem "scout_apm"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
